28f2bf9cf81dcf87e9b81095ab1f486f80de93c3
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2020-11-26 07:05:25 +0330

    Change dark/light color mode to icons

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,11 +7,31 @@ import {
 	useColorMode,
 	Button,
 	Set,
+	Icon,
 } from 'bumbag';
 
 const Header = () => {
 
 	const { colorMode, setColorMode } = useColorMode();
+
+	const moon = {
+		viewBoxWidth: 24,
+		viewBoxHeight: 24,
+		paths: ['M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z']
+	};
+
+	const sun = {
+		viewBoxWidth: 24,
+		viewBoxHeight: 24,
+		paths: ['M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z'],
+	};
+
+	const isDark = () => colorMode === 'dark';
+
+	const toggleColorMode = () => {
+		if (isDark()) setColorMode('light');
+		else setColorMode('dark');
+	};
 
 	return (
 		<TopNav>
@@ -24,8 +44,9 @@ const Header = () => {
 			<TopNav.Section>
 				<TopNav.Item>
 					<Set>
-						<Button onClick={() => setColorMode('default')}>Light</Button>
-						<Button onClick={() => setColorMode('dark')}>Dark</Button>
+						<Button onClick={() => toggleColorMode()} variant="ghost" size="large">
+							<Icon aria-label='moon' icon={isDark() ? sun : moon} fontSize="400" />
+						</Button>
 					</Set>
 				</TopNav.Item>
 			</TopNav.Section>
